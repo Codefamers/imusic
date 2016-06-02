6167259c5c771bbb7454af053b626291b0e1acd2
quhuainan <2219890359@qq.com>
2016-06-02 17:07:07 +0800

    完成基本布局界面

--- app/src/main/java/com/example/admins/imusic/activity/SearchMusicActivity.java
+++ app/src/main/java/com/example/admins/imusic/activity/SearchMusicActivity.java
@@ -5,17 +5,39 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.admins.imusic.R;
 
-public class SearchMusicActivity extends AppCompatActivity {
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
 
+@ContentView(R.layout.activity_search_music)
+public class SearchMusicActivity extends AppCompatActivity {
+    @ViewInject(R.id.toolbar)
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_music);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        x.view().inject(this);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white);
+        toolbar.inflateMenu(R.menu.search_toolbar_menu);
+        /*toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SearchMusicActivity.this, "点击返回按钮", Toast.LENGTH_SHORT).show();
+            }
+        });*/
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(SearchMusicActivity.this, "点击搜索框", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
